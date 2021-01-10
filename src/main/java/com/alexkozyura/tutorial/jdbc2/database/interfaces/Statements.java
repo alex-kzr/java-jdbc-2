9806d2c1ba7f7ee24b3e373dfb28e76a59967b78
Alex-SK <aleksey.kozyura@gmail.com>
2021-01-10 17:10:27 +1000

    Add Statements interface

---
+++ src/main/java/com/alexkozyura/tutorial/jdbc2/database/interfaces/Statements.java
@@ -0,0 +1,11 @@
+package com.alexkozyura.tutorial.jdbc2.database.interfaces;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface Statements {
+
+    PreparedStatement getStatement(long id) throws SQLException;
+
+    PreparedStatement getStatementAll() throws SQLException;
+}
